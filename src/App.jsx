5eba5d400cdcee609a3928ff905ba33374fd26a7
Robby94gtr <robertogramazio94@gmail.com>
2025-05-21 01:39:34 +0200

    [UPD] Created logic form sending to endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
 
   const [formData, setFormData] = useState({
-    name: "",
     title: "",
-    text: "",
+    author: "",
+    body: "",
     public: ""
   })
+
+
+  const sendData = (e) => {
+    e.preventDefault();
+    axios.post('https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts', formData).then((resp) => { console.log(resp.data) })
+    setFormData({
+      ...formData
+    })
+
+  }
+
 
 
   return (
@@ -20,27 +32,33 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col-12 mt-5">
-              <div className='mb-2'>Autore</div>
-              <input className="form-control form-control-lg" type="text" placeholder="Autore del post" aria-label=".form-control-lg example" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
-              <div className='mt-2'  >Titolo</div>
-                <input className="form-control mt-2" type="text" placeholder="Titolo del post" aria-label="default input example" value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
-              <div class="mt-3">
-                <label for="exampleFormControlTextarea1" class="form-label"> Inserisci qui sotto il testo del post</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={formData.text} onChange={(e) => setFormData({ ...formData, text: e.target.value })}></textarea>
-              </div>
-              <div class="form-check mt-3">
-                <input class="form-check-input" type="checkbox" value="" id="checkDefault" />
-                <label class="form-check-label" for="checkDefault">
-                  Pubblica il post
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="checkChecked" />
-                <label class="form-check-label" for="checkChecked">
-                  Salva come bozza
-                </label>
-              </div>
+              <form onSubmit={sendData} >
+                <div className='mb-2'>Autore</div>
+                <input className="form-control form-control-lg" type="text" placeholder="Autore del post" aria-label=".form-control-lg example" name='autore' value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+
+                <div className='mt-2'  >Titolo</div>
+                <input className="form-control mt-2" type="text" placeholder="Titolo del post" aria-label="default input example" name='titolo' value={formData.author} onChange={(e) => setFormData({ ...formData, author: e.target.value })} />
+
+                <div className="mt-3">
+                  <label htmlFor="exampleFormControlTextarea1" className="form-label"> Inserisci qui sotto il testo del post</label>
+                  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" name='testo' value={formData.body} onChange={(e) => setFormData({ ...formData, body: e.target.value })}></textarea>
+                </div>
+                <div className="form-check mt-3">
+                  <input className="form-check-input" type="checkbox" value="" id="checkDefault" />
+                  <label className="form-check-label" htmlFor="checkDefault">
+                    Pubblica il post
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="checkChecked" />
+                  <label className="form-check-label" htmlFor="checkChecked">
+                    Salva come bozza
+                  </label>
+                </div>
+                <button className='btn btn-primary'>Invia</button>
+              </form>
             </div>
+            
           </div>
         </div>
       </main>
