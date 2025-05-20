8cf4444c35e3ec219a92f0a660ac32007c4488ac
Robby94gtr <robertogramazio94@gmail.com>
2025-05-21 01:35:31 +0200

    [UPD] Created variable to fetch form's data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    name: "",
+    title: "",
+    text: "",
+    public: ""
+  })
+
 
   return (
     <>
@@ -14,12 +21,12 @@ function App() {
           <div className="row">
             <div className="col-12 mt-5">
               <div className='mb-2'>Autore</div>
-              <input className="form-control form-control-lg" type="text" placeholder="Autore del post" aria-label=".form-control-lg example" />
+              <input className="form-control form-control-lg" type="text" placeholder="Autore del post" aria-label=".form-control-lg example" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
               <div className='mt-2'  >Titolo</div>
-              <input className="form-control mt-2" type="text" placeholder="Titolo del post" aria-label="default input example" />
+                <input className="form-control mt-2" type="text" placeholder="Titolo del post" aria-label="default input example" value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
               <div class="mt-3">
                 <label for="exampleFormControlTextarea1" class="form-label"> Inserisci qui sotto il testo del post</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={formData.text} onChange={(e) => setFormData({ ...formData, text: e.target.value })}></textarea>
               </div>
               <div class="form-check mt-3">
                 <input class="form-check-input" type="checkbox" value="" id="checkDefault" />
@@ -28,7 +35,7 @@ function App() {
                 </label>
               </div>
               <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="checkChecked" checked />
+                <input class="form-check-input" type="checkbox" value="" id="checkChecked" />
                 <label class="form-check-label" for="checkChecked">
                   Salva come bozza
                 </label>
