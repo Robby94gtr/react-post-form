9a9b117d9210227adee64aa17d0aeb0d159badeb
Robby94gtr <robertogramazio94@gmail.com>
2025-05-21 01:42:26 +0200

    [UPD] Created Radio Button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     title: "",
     author: "",
     body: "",
-    public: ""
+    public: false
   })
 
 
@@ -33,24 +33,24 @@ function App() {
           <div className="row">
             <div className="col-12 mt-5">
               <form onSubmit={sendData} >
-                <div className='mb-2'>Autore</div>
-                <input className="form-control form-control-lg" type="text" placeholder="Autore del post" aria-label=".form-control-lg example" name='autore' value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+                <div className='mb-2'>Titolo</div>
+                <input className="form-control form-control-lg" type="text" placeholder="Titolo del post" aria-label=".form-control-lg example" name='autore' value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
 
-                <div className='mt-2'  >Titolo</div>
-                <input className="form-control mt-2" type="text" placeholder="Titolo del post" aria-label="default input example" name='titolo' value={formData.author} onChange={(e) => setFormData({ ...formData, author: e.target.value })} />
+                <div className='mt-2'  >Autore</div>
+                <input className="form-control mt-2" type="text" placeholder="Autore del post" aria-label="default input example" name='titolo' value={formData.author} onChange={(e) => setFormData({ ...formData, author: e.target.value })} />
 
                 <div className="mt-3">
                   <label htmlFor="exampleFormControlTextarea1" className="form-label"> Inserisci qui sotto il testo del post</label>
                   <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" name='testo' value={formData.body} onChange={(e) => setFormData({ ...formData, body: e.target.value })}></textarea>
                 </div>
                 <div className="form-check mt-3">
-                  <input className="form-check-input" type="checkbox" value="" id="checkDefault" />
+                  <input className="form-check-input" type="radio" name='visibility' value="" id="pubblica" checked={formData.public === false} onChange={(e) => setFormData({ ...formData, public: false })} />
                   <label className="form-check-label" htmlFor="checkDefault">
                     Pubblica il post
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" id="checkChecked" />
+                  <input className="form-check-input" type="radio" name='visibility' value="" id="bozza" checked={formData.public === true} onChange={(e) => setFormData({ ...formData, public: true })} />
                   <label className="form-check-label" htmlFor="checkChecked">
                     Salva come bozza
                   </label>
@@ -58,7 +58,7 @@ function App() {
                 <button className='btn btn-primary'>Invia</button>
               </form>
             </div>
-            
+
           </div>
         </div>
       </main>
